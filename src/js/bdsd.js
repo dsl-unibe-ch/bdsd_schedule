c32b31128e06a516dbb1505a451f5deedd6fa895
Alexander Kashev <alexander.kashev@unibe.ch>
2023-05-04 06:56:37 +0200

    Purge temporary JS

--- src/js/bdsd.js
+++
@@ -1,6 +0,0 @@
-// Kill all service workers
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for(let registration of registrations) {
-        registration.unregister();
-    } 
-});
